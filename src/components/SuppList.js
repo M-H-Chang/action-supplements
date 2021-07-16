c92748383e9d91a53c7d6490dff5a3f5681dffa7
Min Chang <minchangmhc@gmail.com>
2021-07-16 13:22:29 -0700

    ✨ add supplement list component.

--- src/components/SuppList.js
+++ src/components/SuppList.js
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { useFirestoreConnect, isLoaded } from 'react-redux-firebase'
 import { Link, useLocation } from "react-router-dom"
+import queryString from 'query-string'
 
 const SuppList = () => {
   const{ search } = useLocation()
@@ -16,10 +17,32 @@ const SuppList = () => {
 
   const supplements = useSelector(state => state.firestore.ordered.supplements)
 
-  
+
   return (
     <>
-
+      {isLoaded(supplements)
+      ? supplements.map(supplement => {
+        const { title, price, description, id } = supplement
+        return (
+          <div
+            className={selectedSupplementsId === id ? 'selected' : 'unselected'}
+            key={id}  
+            title={title}
+            price={price}
+            description={description}
+            onClick={() => setSelectedSupplementsId(id)}
+          >
+            <h2>{title}</h2>
+            <p>{price}</p>
+            <p>{description}</p>
+          </div>
+        )
+        
+      })
+      : (<h3>Loading...</h3>)
+     }
     </>
   )
 }
+
+export default SuppList;
