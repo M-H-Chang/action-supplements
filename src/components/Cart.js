3ffe9f55d64f99572d9619d7477d7cab2be871aa
Min Chang <minchangmhc@gmail.com>
2021-07-23 14:30:35 -0700

    ✨ add cart component.

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import {
+  useFirestore,
+  useFirestoreConnect,
+  isLoaded
+} from 'react-redux-firebase'
+import { useParams } from 'react-router';
 
-function Cart() {
+const Cart = () => {
+  const history = useHistory()
+  const firestore = useFirestore()
+  const { id: selectedSuppId } = useParams()
 
+  useFirestoreConnect([
+    { collection: 'cartSupplments'}
+  ])
 
+  const deleteSupplement = () => {
+    history.push('/supplements/cart') //need to make a route for the cart
+    return firestore.delete({ collection: 'cartSupplements', doc: selectedSuppId })
+  }
+
+  const supplement = useSelector(
+    state => state.firestore.data.cartSupplements[selectedSuppId]
+  )
   return (
     <>
-      
+      <h4>Current Cart</h4>
+      <p>{title}</p>
+      <p>{description}</p>
+      <p>{price}</p>
+      <button type='submit'>Checkout</button>
     </>
   )
 }
