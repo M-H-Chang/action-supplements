b4c152292b27d9c47dfd2f5d069dc2cef36e3a53
Min Chang <minchangmhc@gmail.com>
2021-07-23 10:00:19 -0700

    ✨ add details component.

---
+++ src/components/SuppControl.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Router
+} from 'react-router-dom'
+
+import SuppList from './SuppList'
+import SuppEdit from './SuppEdit'
+import SuppDetail from './SuppDetail'
---
+++ src/components/SuppDetail.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { useParams, Link, useHistory } from 'react-router-dom'
+import {
+  useFirestore,
+  useFirestoreConnect,
+  isLoaded,
+} from 'react-redux-firebase'
+
+function SuppDetail() {
+  const history = useHistory()
+  const firestore = useFirestore()
+  const { id: selectedSuppId } = useParams() //has to match selectedSuppId route
+
+  useFirestoreConnect([
+    { collection: 'supplements'}
+  ])
+
+  const deleteSupplement = () => {
+    history.push('/supplements')
+    return firestore.delete({ collection: 'surveys', doc: selectedSuppId})
+  }
+
+  const supplement = useSelector(
+    state => state.firestore.data.supplements[selectedSuppId]
+  )
+
+  const { title, description, price } = supplement
+
+  return (
+    <>
+      {isLoaded(supplement) ?
+      (
+        <>
+          <Link to={`/supplements/${selectedSuppId}/edit`}>
+            <h2>{title}</h2>
+            <p>{description}</p>
+            <p>{price}</p>
+          </Link>
+          <button type='button' onClick={() => deleteSupplement()}>Delete</button>
+        </>
+      )
+      : <h3>Loading...</h3>
+      }
+    </>
+  )
+}
+
+export default SuppDetail
