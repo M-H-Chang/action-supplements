9ece1b962e5a625ed5c3b8cdd6b6f0b7f958e1fe
Min Chang <minchangmhc@gmail.com>
2021-07-27 17:30:14 -0700

    🚧 redux bug.

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,25 @@
 import SuppControl from './SuppControl'
+import { useGetAllSupplementsQuery } from "../store/supplementApi"
+import { Link } from "react-router-dom"
 
-const App = () => <SuppControl />
+const App = () => {
+  <SuppControl />
+  const {
+    data: allSupplements,
+    error: allSupplementsError,
+    isLoading: allSupplementsIsLoading
+  } = useGetAllSupplementsQuery();
+  if (allSupplementsIsLoading) return <div>Loading...</div>;
+  if (allSupplementsError) return <div>Unable to load supplements.</div>
+  return (
+    <ul>
+      {allSupplements.map((supplement, idx) => (
+        <li key={idx}>
+          <Link to={`/details/${supplement.id}`}>{supplement.name}</Link>
+        </li>
+      ))}
+    </ul>
+  )
+} 
 
 export default App
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "./counterSlice";
-import supplementApi from "./supplementApi";
+import { setupListeners } from '@reduxjs/toolkit/query'
+import { supplementApi } from "./supplementApi";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     [supplementApi.reducerPath]: supplementApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(supplementApi.middleware),
 });
+setupListeners(store.dispatch)
